77083804814aabf01973d009c02b27ebe95cac28
macfisher <macfisher01@gmail.com>
2017-05-04 17:04:47 -0600

    Added AccessDropDown code snippet

---
+++ AccessDropDown.java
@@ -0,0 +1,23 @@
+package newpackage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.By;
+
+public class AccessDropDown {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.firefox.marionette","C:\\geckodriver.exe");
+	    String baseUrl = "http://newtours.demoaut.com/mercuryregister.php";
+	    WebDriver driver = new FirefoxDriver();
+		driver.get(baseUrl);
+	
+		Select drpCountry = new Select(driver.findElement(By.name("country")));
+		drpCountry.selectByVisibleText("ANTARCTICA");
+	
+		//Selecting Items in a Multiple SELECT elements
+		driver.get("http://jsbin.com/osebed/2");
+		Select fruits = new Select(driver.findElement(By.id("fruits")));
+		fruits.selectByVisibleText("Banana");
+		fruits.selectByIndex(1);
+	 }
+}
