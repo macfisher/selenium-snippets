44b71f2f562fffc67a8b06bf00367299c01ff54f
macfisher <macfisher01@gmail.com>
2017-05-04 17:57:27 -0600

    Added AllLinks code snippet

---
+++ AllLinks.java
@@ -0,0 +1,41 @@
+package newpackage;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class AllLinks {
+	public static void main(String[] args) {
+	 	System.setProperty("webdriver.firefox.marionette","C:\\geckodriver.exe");
+        String baseUrl = "http://newtours.demoaut.com/";
+        WebDriver driver = new FirefoxDriver();
+        String underConsTitle = "Under Construction: Mercury Tours";
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        driver.get(baseUrl);
+        List<WebElement> linkElements = driver.findElements(By.tagName("a"));
+        String[] linkTexts = new String[linkElements.size()];
+        int i = 0;
+
+        //extract the link texts of each link element
+        for (WebElement e : linkElements) {
+            linkTexts[i] = e.getText();
+            i++;
+        }
+
+        //test each link
+        for (String t : linkTexts) {
+            driver.findElement(By.linkText(t)).click();
+            if (driver.getTitle().equals(underConsTitle)) {
+                System.out.println("\"" + t + "\""
+                        + " is under construction.");
+            } else {
+                System.out.println("\"" + t + "\""
+                        + " is working.");
+            }
+            driver.navigate().back();
+        }
+        driver.quit();
+    }
+}
