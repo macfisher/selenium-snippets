256be9d3ee8a66365f580e489ce380a9702d7172
macfisher <macfisher01@gmail.com>
2017-05-08 16:50:07 -0600

    Moved comments/notes to external .txt doc

--- IEExample.java
+++ IEExample.java
@@ -3,37 +3,14 @@ package newpackage;
 import org.openqa.selenium.*;
 import org.openqa.selenium.ie.*;
 import org.openqa.selenium.support.ui.*;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class IEExample {
-	/**
-	,-------------------------.
-	| IEDriverServer Settings |
-	|-----------------------------------------------------------------------------------------------.
-	|                                                                                               |
-	| It needs to set same Security level in all zones:                                             |
-	|                                                                                               |
-	| 1.Open IE                                                                                     |
-	| 2.Go to Tools -> Internet Options -> Security                                                 |
-	| 3.Set all zones (Internet, Local intranet, Trusted sites, Restricted sites)                   |
-	|   to the same protected mode, enabled or disabled should not matter.                          |
-	|                                                                                               |  
-	| Finally, set Zoom level to 100% by right clicking on the gear located at the top right corner | 
-	| and enabling the status-bar. Default zoom level is now displayed at the lower right.          |
-	| These settings will most likely require a reboot of Windows. I suspect registry values        |
-	| were set in the process.                                                                      |
-	|                                                                                               |
-	| NOTE: Running IE11 on Windows 7. Edge only runs on Windows 10.                                |
-	|                                                                                               |
-	| IEDriverServer (64bit) has a bug with sendKeys() function executing very slowly, use 32bit    |
-	| https://github.com/SeleniumHQ/selenium/wiki/InternetExplorerDriver                            |
-	|                                                                                               |
-	`-----------------------------------------------------------------------------------------------'
-	 */
-	
-	
 	public static void main(String[] args) {
 		System.setProperty("webdriver.ie.driver", "C:\\IEDriverServer.exe");
+		
 		WebDriver driver = new InternetExplorerDriver();
+		
 		driver.get("https://www.google.com");
 		
 		WebDriverWait wait = new WebDriverWait(driver, 3);
